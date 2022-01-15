efd7e2adaf483d23addfd050c5412e7c3d8b7179
sujk912 <aa991122aa@gmail.com>
2022-01-15 23:24:15 +0900

    문제생성 페이지 디자인 완료 (약간의 js포함)

--- isThatRight/createGame/static/js/selectGame.js
+++ isThatRight/createGame/static/js/selectGame.js
@@ -1,1 +1,26 @@
-console.log("object");
+// constant -------------------------
+const gameType = ['tt','ts','it','is'];
+
+// Dom Element -------------------------
+const selectTypeInputs = document.querySelectorAll("input[name='selectType']");
+const imageFileInputs =document.querySelectorAll(".textQuestion input[type='file']");
+const gameCountSelector = document.querySelector("#gameCountSelector");
+const addQnaBtn =document.querySelector(".addQnaButton");
+// initialize -------------------------
+// add game function
+for(let i=1;i<=10;i++){
+    let option = document.createElement(`option`);
+    option.value = i;
+    option.text =`${i}번`;
+    gameCountSelector.appendChild(option);
+}
+// game type
+selectTypeInputs.forEach(radio => radio.addEventListener('click',function(){
+    let selected = document.querySelector("input[name='selectType']:checked").value;
+    makeGameParts.forEach(p=> p.classList.add('hidden'));
+    makeGameParts[gameType.indexOf(selected)].classList.remove('hidden');
+}));
+imageFileInputs.forEach(uploader => uploader.addEventListener('change',function(e){
+    const selectedLabel = document.querySelector(`.textQuestion label[for='${e.target.id}']`);
+    selectedLabel.innerHTML = e.target.files[0].name;
+}));
